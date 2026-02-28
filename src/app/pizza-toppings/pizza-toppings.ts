7122e8be6a50aa499304da1b690ff7af54b5c868
tsteelematc <tsteele@madisoncollege.edu>
2026-02-28 12:53:48 -0600

    checkbox hooked, or bound to the signal data

--- src/app/pizza-toppings/pizza-toppings.ts
+++ src/app/pizza-toppings/pizza-toppings.ts
@@ -1,5 +1,5 @@
 import { Component, inject, signal } from '@angular/core';
-import { PizzaService } from '../pizza.service';
+import { PizzaService, PizzaTopping } from '../pizza.service';
 import { CurrencyPipe } from '@angular/common';
 
 @Component({
@@ -16,6 +16,17 @@ export class PizzaToppings {
 
   protected readonly availablePizzaToppings = signal(
     this.pizzaSvc.getAvailablePizzaToppings()
+  );
+
+  protected readonly toggleTopping = (t: PizzaTopping) => this.availablePizzaToppings.update(
+    prev => prev.map(
+      x => x === t
+        ? {
+          ...x,
+          checked: !x.checked
+        }
+        : x
+    )
   );
 
 }
